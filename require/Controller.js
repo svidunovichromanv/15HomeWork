0a6b27e11508e278b1acdf5f2d42d84d2dc2aeb1
svidunovichromanv <svidunovichromanv@gmail.com>
2018-08-04 14:55:34 +0300

    first commit

---
+++ require/Controller.js
@@ -0,0 +1,19 @@
+define(function() {
+    class Controller {
+    constructor(model, view) {
+        this.view = view;
+        let self = this;
+        model.changeCallback = function() {
+            self.renderView();
+        };
+        view.onCheckedCallback = function(isChecked) {
+            model.start(isChecked);
+        }
+    }
+
+    renderView() {
+        this.view.render();
+    }
+}
+    return Controller;
+});
